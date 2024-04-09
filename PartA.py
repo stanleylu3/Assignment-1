a6343e52888d9c43de09b7a88dd5d46b5f36ef84
Stanley Lu <stanlehl@uci.edu>
2024-04-08 22:44:31 -0700

    Added functionality for computeWordFrequencies.

--- PartA.py
+++ PartA.py
@@ -11,12 +11,24 @@ def tokenize(TextFilePath) -> list:
                     result = ''.join(char for char in word if char.isalnum() and char.isascii())
                     if result:
                         tokenList.append(result)
+
             return tokenList
+
     except FileNotFoundError:
         print("File not found")
 
-def computeWordFrequencies(tokenList):
-    pass
+def computeWordFrequencies(tokenList) -> dict:
+    frequencies = {}
+    #runs through all the tokens in the given list
+    for token in tokenList:
+        #adds 1 to the counter if the entry exists
+        if token in frequencies:
+            frequencies[token] += 1
+        #creates the k/v pair if entry does not exist
+        else:
+            frequencies[token] = 1
+
+    return frequencies
 
 def printFrequencies(wordCount):
     pass
