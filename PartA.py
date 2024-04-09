82ac6c494cf51cb1b0f7c19247fd8a92f5d8b50e
Stanley Lu <stanlehl@uci.edu>
2024-04-08 19:18:45 -0700

    Added implementation for tokenize method.

--- PartA.py
+++ PartA.py
@@ -1,5 +1,21 @@
-def tokenize(TextFilePath):
-    pass
+def tokenize(TextFilePath) -> list:
+    tokenList = []
+    #attempts to open a file with the given filepath
+    try:
+        with open(TextFilePath, 'r') as file:
+            for line in file:
+                words = line.split()
+                #adds the words if they are unique and alphanumeric
+                for word in words:
+                    #strips non-alphanumeric characters from words
+                    result = ''.join(filter(str.isalnum, word)).lower()
+                    if result not in tokenList:
+                        tokenList.append(result)
+                    else:
+                        continue
+            return tokenList
+    except FileNotFoundError:
+        print("File not found")
 
 def computeWordFrequencies(tokenList):
     pass
