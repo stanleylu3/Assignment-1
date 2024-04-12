96019ea4b2e327d8e81615b47af82c1d54d9c703
Stanley Lu <stanlehl@uci.edu>
2024-04-12 02:40:31 -0700

    Added more exception handling and comments for the all the methods.

--- PartA.py
+++ PartA.py
@@ -1,5 +1,15 @@
 import sys
 def tokenize(TextFilePath: str) -> list:
+    """
+    Time Complexity: This method has a time complexity of O(n) since the time to read data is
+    based on how large the file is. Reading bytes, splitting the lines, and appending them to
+    the tokenList are all based on how large the file is.
+
+    Takes a text file and returns a list of alphanumeric tokens, independent of capitalization.
+
+    :param TextFilePath: A string that contains a path to a text file.
+    :return: A list of tokens in the text file.
+    """
     tokenList = []
     temp = ""
     #attempts to open a file with the given filepath
@@ -33,11 +43,22 @@ def tokenize(TextFilePath: str) -> list:
         return tokenList
 
     except FileNotFoundError:
-        print("File not found")
+        pass
+    except UnicodeDecodeError:
+        pass
     except OSError as e:
         print(f"OS error: {e}")
 
 def computeWordFrequencies(tokenList: list) -> dict:
+    """
+    Time Complexity: This method has a time complexity of O(n), since loop will only
+    iterate as "n" times based on the length of tokenList.
+
+    Counts the number of occurrences of each token and returns them in a dictionary.
+
+    :param tokenList: list of tokens
+    :return: dictionary, maps each token to frequency of occurrences
+    """
     frequencies = {}
     #runs through all the tokens in the given list
     for token in tokenList:
@@ -51,6 +72,14 @@ def computeWordFrequencies(tokenList: list) -> dict:
     return frequencies
 
 def printFrequencies(wordCount: dict):
+    """
+    Time complexity: This method is O(n log n), since the builtin function sorted()
+    in Python has a worse case time complexity of O(n log n).
+
+    Prints out the word frequency count, highest to lowest.
+
+    :param wordCount: dictionary, mapping each token to number of frequencies
+    """
     #sorts the dictionary based on the highest frequency word
     sorted_dict = sorted(wordCount.items(), key = lambda x: (-x[1], x[0]))
     for word, freq in sorted_dict:
@@ -58,8 +87,10 @@ def printFrequencies(wordCount: dict):
 
 
 if __name__ == '__main__':
-    filePath = sys.argv[1]
-    tokens = tokenize(filePath)
-    freqCount = computeWordFrequencies(tokens)
-    printFrequencies(freqCount)
-
+    try:
+        filePath = sys.argv[1]
+        tokens = tokenize(filePath)
+        freqCount = computeWordFrequencies(tokens)
+        printFrequencies(freqCount)
+    except TypeError as e:
+        print("File not found or does not exist.")
--- PartB.py
+++ PartB.py
@@ -1,7 +1,20 @@
 import PartA
 import sys
 
-def countCommonTokens(file1_tokens, file2_tokens):
+def countCommonTokens(file1_tokens: dict, file2_tokens: dict):
+    """
+    Time Complexity: This method has a time complexity of O(n) since converting each dict
+    to a set is based on the size of the dict, or "n", and doing twice is 2n, which simplifies
+    to O(n). Comparing the two sets is based on the size of the smaller set, which is still
+    O(n) since the set size is based of the dictionary size.
+
+    Returns the number of common tokens between two files.
+
+    :param file1_tokens: A dict containing tokens of first file passed.
+    :param file2_tokens: A dict containing tokens of second file passed.
+    :return: A list of the common tokens between the two files and the int of the number of
+    common tokens.
+    """
     tokens1 = set(file1_tokens.keys())
     tokens2 = set(file2_tokens.keys())
 
@@ -10,11 +23,16 @@ def countCommonTokens(file1_tokens, file2_tokens):
     return commonTokens, len(commonTokens)
 
 if __name__ == "__main__":
-    file1 = sys.argv[1]
-    file2 = sys.argv[2]
+    try:
+        file1 = sys.argv[1]
+        file2 = sys.argv[2]
 
-    file1Dict = PartA.computeWordFrequencies(PartA.tokenize(file1))
-    file2Dict = PartA.computeWordFrequencies(PartA.tokenize(file2))
-    tokens, count = countCommonTokens(file1Dict, file2Dict)
-    print(f"Number of Common Tokens: {count}.")
-    print(f"Common Tokens: {tokens}.")
+        file1Dict = PartA.computeWordFrequencies(PartA.tokenize(file1))
+        file2Dict = PartA.computeWordFrequencies(PartA.tokenize(file2))
+        tokens, count = countCommonTokens(file1Dict, file2Dict)
+        print(f"Number of Common Tokens: {count}.")
+        #print(f"Common Tokens: {tokens}.")
+    except IndexError as e:
+        print("Needs two files to compare")
+    except TypeError as e:
+        print("File not found or does not exist.")
