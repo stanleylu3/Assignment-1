926aaab19b8a4e3c3399ff0f0ee50fc562ea1188
Stanley Lu <stanlehl@uci.edu>
2024-04-11 17:51:17 -0700

    Changed PartA to read by 256 bytes instead of line by line.

--- PartA.py
+++ PartA.py
@@ -1,19 +1,36 @@
 import sys
 def tokenize(TextFilePath: str) -> list:
     tokenList = []
+    temp = ""
     #attempts to open a file with the given filepath
     try:
         with open(TextFilePath, 'r', encoding = 'utf-8') as file:
-            for line in file:
-                words = line.split()
-                #adds the words if they are alphanumeric
-                for word in words:
-                    #strips non-alphanumeric characters from words
-                    result = ''.join(char for char in word if char.isalnum() and char.isascii())
-                    if result:
-                        tokenList.append(result.lower())
+            while True:
+                #read files in chunks of 256 bytes
+                data = file.read(256)
+                #breaks out of loops when there is no data left to read
+                if not data:
+                    break
 
-            return tokenList
+                temp += data
+                lines = temp.split('\n')
+                temp = lines.pop() if lines else ""
+
+                for line in lines:
+                    words = line.split()
+                    #adds the words if they are alphanumeric
+                    for word in words:
+                        #strips non-alphanumeric characters from words
+                        result = ''.join(char for char in word if char.isalnum() and char.isascii())
+                        if result:
+                            tokenList.append(result.lower())
+            #processes remaining temp data
+            words = temp.split()
+            for word in words:
+                result = ''.join(char for char in word if char.isalnum() and char.isascii())
+                if result:
+                    tokenList.append(result.lower())
+        return tokenList
 
     except FileNotFoundError:
         print("File not found")
